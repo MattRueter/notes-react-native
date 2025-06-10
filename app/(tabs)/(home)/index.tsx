8dd86e67be6e8cebd795a538f78a06dd07ab0f04
Matt <84277551+MattRueter@users.noreply.github.com>
2025-06-10 14:23:13 -0500

    add not found page.

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        gap: 20
       }}
     >
       <Text>Here are your notes.</Text>
+      <Link style={styles.linkBtn} href="err">...where does this lead to?"</Link> 
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  linkBtn: {
+    color:"blue",
+  }
+})
