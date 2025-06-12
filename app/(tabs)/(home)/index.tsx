59268199fc21e9b9a50034c2fb34410f3836c758
Matt <84277551+MattRueter@users.noreply.github.com>
2025-06-12 12:35:56 -0500

    fetch notes from api and add basic styles.

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,25 +1,81 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from "react-native";
+
+type Note = {
+  id: string;
+  title: string;
+  content: string;
+  created_at: string;
+}
 
 export default function Index() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState<Note[]>([])
+  
+  const getNotes = async () => {
+    try{
+      const response = await fetch('http://localhost:5000/notes');
+      const notes = await response.json();
+      setData(notes)
+    }catch(err){
+      console.error(err);
+    }finally{
+      setIsLoading(false)
+    }
+  
+  }
 
+  useEffect (() =>{
+    getNotes()
+  },[]);
+  
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        gap: 20
+        gap: 20,
+        marginTop:80
       }}
     >
-      <Text>Here are your notes.</Text>
-      <Link style={styles.linkBtn} href="err">...where does this lead to?"</Link> 
+      {isLoading ? (
+        <ActivityIndicator/>
+      ):(
+        <>
+          <Text style={styles.heading}>Here are your notes</Text>
+          <FlatList
+            data={data}
+            keyExtractor={( {id} ) => id}
+            renderItem={( {item} ) => (
+              <View style={styles.card}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.content}>{item.content}</Text>
+              </View>
+            )}
+          />
+        </>
+      )}
+
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  linkBtn: {
-    color:"blue",
+const styles =StyleSheet.create({
+  heading: {
+    fontWeight:"bold",
+    fontSize:30
+  },
+  card:{
+    padding: 10,
+    borderWidth:1,
+    margin:10
+  },
+  title:{
+    fontWeight:"bold",
+    fontSize:20,
+  },
+  content:{
+    color:"slate"
   }
 })
